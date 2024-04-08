377db9b1b67c44cf38fedd89be1c9b6aae502534
Spencer <corkransp@proctoracademy.org>
2024-04-07 21:23:35 -0400

    away

---
+++ 4-8 Class Exercises/4-8.js
@@ -0,0 +1,21 @@
+//Try to fix the functions below!
+
+//This function declaration has an error that is preventing the computer from
+//recognizing the declaration
+multiply(p1, p2) {
+  return p1 * p2;
+}
+
+console.log(multiply(2,8)); //should output 16
+
+function divide(dividend) {
+    return dividend / divisor;
+}
+console.log(divide(3,1)); //should output 3
+
+//This function has an error that prevents it from returning a value.
+function toCelsius(fahrenheit) {
+  (5/9) * (fahrenheit-32);
+}
+console.log(toCelsius(212)); //should output 100
+console.log(toCelsius(32)); //should output 0
