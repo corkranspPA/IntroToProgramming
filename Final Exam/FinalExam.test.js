15ce6f05b2764b1faf3df44a78577bcb6a91a34c
corkranspPA <corkransp@proctoracademy.org>
2024-11-18 21:35:19 -0500

    FinalExam Fall24 test file

--- Final Exam/FinalExam.test.js
+++ Final Exam/FinalExam.test.js
@@ -1,14 +1,17 @@
 const fe = require('./FinalExam.js');
 
-describe('divisibleBy', () => {
-  test('4, 2 -> true', () => {
-    expect(fe.divisibleBy(4,2)).toBe(true);
+describe('helloName', () => {
+  test('Alice -> Hello, Alice!', () => {
+    expect(fe.helloName('Alice')).toBe('Hello Alice!');
   });
-  test('5, 2 -> false', () => {
-    expect(fe.divisibleBy(5,2)).toBe(false);
+  test('Bob -> Hello, Bob!', () => {
+    expect(fe.helloName('Bob')).toBe('Hello Bob!');
   });
-  test('25, 5 -> true', () => {
-    expect(fe.divisibleBy(25,5)).toBe(true);
+  test('Cthulu, the Great Dreamer -> Hello Cthulu, the Great Dreamer!', () => {
+    expect(fe.helloName('Cthulu, the Great Dreamer')).toBe('Hello Cthulu, the Great Dreamer!');
+  });
+  test(' -> Hello, !', () => {
+    expect(fe.helloName('')).toBe('Hello !');
   });
 });
 describe('diff21', () => {
@@ -21,41 +24,59 @@ describe('diff21', () => {
   test('21 -> 0', () => {
     expect(fe.diff21(21)).toBe(0);
   });
-});
-describe('positiveNegative', () => {
-  test('1, -1 -> true', () => {
-    expect(fe.positiveNegative(1, -1)).toBe(true);
-  });
-  test('-1, 1 -> true', () => {
-    expect(fe.positiveNegative(-1, 1)).toBe(true);
-  });
-  test('-4, -5 -> false', () => {
-    expect(fe.positiveNegative(-4, -5)).toBe(false);
-  });
-  test('4, 5 -> false', () => {
-    expect(fe.positiveNegative(4, 5)).toBe(false);
+  test('-21 -> 0', () => {
+    expect(fe.diff21(-21)).toBe(42);
   });
 });
-describe('copyEnd', () => {
-  test('Hello -> lololo', () => {
-    expect(fe.copyEnd('Proctor')).toBe('ororor');
+describe('blackjack', () => {
+  test('21, 1 -> 21', () => {
+    expect(fe.blackjack(21, 1)).toBe(21);
   });
-  test('ab -> ababab', () => {
-    expect(fe.copyEnd('ab')).toBe('ababab');
+  test('1, 21 -> 21', () => {
+    expect(fe.blackjack(1, 21)).toBe(21);
   });
-  test('Hi -> HiHiHi', () => {
-    expect(fe.copyEnd('Hi')).toBe('HiHiHi');
+  test('22, 1 -> 1', () => {
+    expect(fe.blackjack(22, 1)).toBe(1);
+  });
+  test('17, 20 -> 20', () => {
+    expect(fe.blackjack(17, 20)).toBe(20);
+  });
+  test('19, 16 -> 19', () => {
+    expect(fe.blackjack(19, 16)).toBe(19);
+  });
+  test('-17, 0 -> 0', () => {
+    expect(fe.blackjack(-17, 0)).toBe(0);
+  });
+});
+describe('biggest', () => {
+  test('1,2,4 -> 4', () => {
+    expect(fe.biggest(1,2,4)).toBe(4);
+  });
+  test('1,2,2 -> 2', () => {
+    expect(fe.biggest(1,2,2)).toBe(2);
+  });
+  test('1,1,1 -> 1', () => {
+    expect(fe.biggest(1,1,1)).toBe(1);
+  });
+  test('-3,-1,-2 -> 1', () => {
+    expect(fe.biggest(-3,-1,-2)).toBe(-1);
   });
 });
 describe('abcOrder', () => {
-  test('1,2,4,false -> true', () => {
-    expect(fe.abcOrder(1, 2, 4, false)).toBe(true);
+  test('1,2,4 -> true', () => {
+    expect(fe.abcOrder(1, 2, 4)).toBe(true);
   });
-  test('1,2,1,false -> false', () => {
-    expect(fe.abcOrder(1, 2, 1, false)).toBe(false);
+  test('1,2,1 -> false', () => {
+    expect(fe.abcOrder(1, 2, 1)).toBe(false);
   });
-  test('1,1,2,true -> true', () => {
-    expect(fe.abcOrder(1, 1, 2, true)).toBe(true);
+  test('1,1,2 -> false', () => {
+    expect(fe.abcOrder(1, 1, 2)).toBe(false);
+  });
+  test('10,100,1000 -> true', () => {
+    expect(fe.abcOrder(10, 100, 1000)).toBe(true);
+  });
+  test('-1000,-100,-10 -> true', () => {
+    expect(fe.abcOrder(-1000, -100, -10)).toBe(true);
   });
 });
 describe('dupliCat', () => {
