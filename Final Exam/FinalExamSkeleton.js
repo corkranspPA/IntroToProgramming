fed9408d09cf6bb6296695009b1975d7f8622785
corkransp <corkransp@proctoracademy.org>
2025-11-19 20:14:58 -0500

    final exam fall 2025

---
+++ Final Exam/FinalExamSkeleton.js
@@ -0,0 +1,30 @@
+//Rename to FinalExam.js!
+
+// Easy - 1 pt
+function greetClass(name) {
+}
+
+function isPassing(grade) {
+}
+
+// Medium - 2 pts
+function in3050(a, b) {
+}
+
+function removeXStart(str) {
+}
+
+// Hard - 3 pts
+function hasSixSeven(str) {
+}
+
+function repeatFront(str, n) {
+}
+
+// Export statements for Jest tests
+exports.greetClass = greetClass;
+exports.isPassing = isPassing;
+exports.in3050 = in3050;
+exports.removeXStart = removeXStart;
+exports.hasSixSeven = hasSixSeven;
+exports.repeatFront = repeatFront;
